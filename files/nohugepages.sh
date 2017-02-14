2cf4803386baa01f6d7ade6ca232c4476a1023ac
bcdonadio <bdonadio@stone.com.br>
2017-02-14 17:34:09 +0000

    Add standalone mode and stop feature

--- files/nohugepages.sh
+++ files/nohugepages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "never" > /sys/kernel/mm/transparent_hugepage/enabled
-echo "never" > /sys/kernel/mm/transparent_hugepage/defrag
-
+case "$1" in
+    stop)
+        echo "madvise" > /sys/kernel/mm/transparent_hugepage/enabled
+        echo "madvise" > /sys/kernel/mm/transparent_hugepage/defrag
+        ;;
+    *)
+        echo "never" > /sys/kernel/mm/transparent_hugepage/enabled
+        echo "never" > /sys/kernel/mm/transparent_hugepage/defrag
+        ;;
+esac
